631334e791c73307b3bba780fdd96e1f19b65a79
Hirisci <www.alan92@icloud.com>
2022-07-12 10:27:12 +0200

    up

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,18 +5,22 @@
 @endsection
 
 @section('content-page')
-    <div class="card">
-        <img src="{{$movie->image}}" alt="{{$movie->title}}" class="card__img">
-        <div class="card__tile">
-            <p class="card__title-primary">{{$movie->title}}</p>
-            <p class="card__title-secondary">{{$movie->original_title}}</p>
-        </div>
-        <div class="card__info">
-            <div>
-                <div class="card__date">{{$movie->date}}</div>
-                <div class="card__vote">{{$movie->vote}}</div>
+<section class="show">
+    <div class="container">
+        <div class="card">
+            <img src="{{$movie->image}}" alt="{{$movie->title}}" class="card__img">
+            <div class="card__title">
+                <p class="card__title-primary">{{$movie->title}}</p>
+                <p class="card__title-secondary">{{$movie->original_title}}</p>
             </div>
-            <div class="card__nationality">{{$movie->nationality}}</div>
+            <div class="card__info">
+                <div>
+                    <div class="card__date">{{$movie->date}}</div>
+                    <div class="card__vote">{{$movie->vote}}</div>
+                </div>
+                <div class="card__nationality">{{$movie->nationality}}</div>
+            </div>
         </div>
     </div>
+</section>
 @endsection
